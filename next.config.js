ddc86c27540b34caae56b085cb222a3cb55127f2
Chinf Hung <chinf.work@gmail.com>
2023-11-01 04:03:08 +0800

    for github page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-}
+  async rewrites() {
+    return {
+      fallback: [
+        // These rewrites are checked after both pages/public files
+        // and dynamic routes are checked
+        {
+          source: "/:path*",
+          destination: `https://chinf-tw.github.io/hcsh-web/:path*`,
+        },
+      ],
+    };
+  },
+  output: "export",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
