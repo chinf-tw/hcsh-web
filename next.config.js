c3eadc6688fa588c9b1b14355757a82a1efa506b
Chinf Hung <chinf.work@gmail.com>
2023-11-01 04:14:49 +0800

    fix: Error: Specified "rewrites" cannot be used with "output: export".

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return {
-      fallback: [
-        // These rewrites are checked after both pages/public files
-        // and dynamic routes are checked
-        {
-          source: "/:path*",
-          destination: `https://chinf-tw.github.io/hcsh-web/:path*`,
-        },
-      ],
-    };
-  },
+  assetPrefix: ".",
   output: "export",
 };
 
